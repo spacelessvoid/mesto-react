5bca35e1e18d0f78520818fe2be26c1f4ea6c8c0
Aleksey Tishenkov <tishenkov@gmail.com>
2023-05-08 16:32:33 +0400

    feat: add user info fetching

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,20 +1,45 @@
-export default function Main({ onEditProfile, onAddPlace, onEditAvatar }) {
+import { useEffect, useState } from "react";
+import { api } from "../utils/Api";
+
+export default function Main({ onEditAvatar, onEditProfile, onAddPlace }) {
+  const [userAvatar, setUserAvatar] = useState("");
+  const [userName, setUserName] = useState("");
+  const [userDescription, setUserDescription] = useState("");
+
+  // Fetching user profile data
+  useEffect(() => {
+    api
+      .getUserInfo()
+      .then(result => {
+        setUserAvatar(result.avatar);
+        setUserName(result.name);
+        setUserDescription(result.about);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, [userAvatar, userName, userDescription]);
+
   return (
     <main className="content">
       <section className="profile" aria-label="Профиль пользователя">
         <div className="profile__avatar-container" onClick={onEditAvatar}>
-          <img src="#" alt="Фото пользователя" className="profile__avatar" />
+          <img
+            src={userAvatar}
+            alt="Фото пользователя"
+            className="profile__avatar"
+          />
         </div>
 
         <div className="profile__info">
-          <h1 className="profile__name">Жак-Ив Кустов</h1>
+          <h1 className="profile__name">{userName}</h1>
           <button
             className="profile__edit-btn button"
             type="button"
             aria-label="Редактировать профиль"
             onClick={onEditProfile}
           ></button>
-          <p className="profile__job">Исследователь кустов</p>
+          <p className="profile__job">{userDescription}</p>
         </div>
 
         <button
