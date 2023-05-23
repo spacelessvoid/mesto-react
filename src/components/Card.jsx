0993e0062ca093a0c4257a0005b1ffa3e51df035
Aleksey Tishenkov <tishenkov@gmail.com>
2023-05-23 16:56:03 +0300

    feat: make like & delete buttons visible only on own cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,24 @@
-export default function Card({ card: { id, link, name, likes }, onCardClick }) {
+import { useContext } from "react";
+import { CurrentUserContext } from "../contexts/CurrentUserContext";
+
+export default function Card({
+  card: { id, link, name, likes, owner },
+  onCardClick,
+}) {
+  const user = useContext(CurrentUserContext);
+
+  const isOwn = owner._id === user._id;
+
+  const isLiked = likes.some(i => i._id === user._id);
+  const cardLikeButtonClassName = `button card__like-btn ${
+    isLiked && "card__like-btn_active"
+  }`;
+
   function handleClick() {
     onCardClick({ link, name });
   }
+
+  function handleDeleteClick() {}
 
   return (
     <article className="gallery__card card" id={id}>
@@ -11,15 +28,18 @@ export default function Card({ card: { id, link, name, likes }, onCardClick }) {
         className="card__image"
         onClick={handleClick}
       />
-      <button
-        className="card__delete-btn button"
-        type="button"
-        aria-label="Удалить фото"
-      ></button>
+      {isOwn && (
+        <button
+          className="card__delete-btn button"
+          type="button"
+          aria-label="Удалить фото"
+          onClick={handleDeleteClick}
+        />
+      )}
       <div className="card__info">
         <h2 className="card__place">{name}</h2>
         <button
-          className="card__like-btn button"
+          className={cardLikeButtonClassName}
           type="button"
           aria-label="Сердечко"
         ></button>
